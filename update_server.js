5212326e2a446966518d97d2a34636a9dc8ac0c4
Haixiang <haixiang6123@gmail.com>
2020-03-14 01:33:14 -0700

    Add comments

--- update_server.js
+++ update_server.js
@@ -3,7 +3,9 @@ const fs = require('fs')
 const server = http.createServer()
 
 server.on('request', (request, response) => {
+    // 文件流
     const stream = fs.createReadStream('./big_file.txt')
+    // 将文件流传到 response，response 也是一个流
     stream.pipe(response)
 })
 
